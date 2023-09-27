6fb2966ee2a4b5b957838ba7b38f41a1abaac560
Gi-onthenight <111322467+Gi-onthenight@users.noreply.github.com>
2023-09-27 18:22:22 +0700

    update v0.1nw.beta

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
 from flask import Flask, render_template, jsonify, request, redirect, url_for, flash, session
 from pymongo import MongoClient
+from dotenv import load_dotenv
 import string
 import random
 import os
+from os.path import join, dirname
 from datetime import datetime
 
-client = MongoClient('mongodb+srv://test:sparta@cluster0.mdmqvsh.mongodb.net/')
-db = client.lapor_lx
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+MONGODB_URI = os.environ.get("MONGODB_URI")
+DB_NAME =   os.environ.get("DB_NAME")
+
+client = MongoClient(MONGODB_URI)
+db = client[DB_NAME]
 
 def generate_random_filename(length):
     letters_and_digits = string.ascii_letters + string.digits
@@ -14,20 +22,24 @@ def generate_random_filename(length):
 
 app = Flask(__name__)
 
-app.config['UPLOAD_FOLDER'] = 'static/img/lampiran'
+app.config['UPLOAD_FOLDER'] = 'static/imgs/lampiran'
 app.secret_key = os.urandom(7)
 
 @app.route('/')
 def index():
+    page = "Lapor LX"
     total_d = db.laporan.count_documents({})
-    return render_template('index.html', total_d=total_d)
+    
+    return render_template('index.html', page=page, total_d=total_d)
 
 @app.route('/laporan')
 def form():
-    return render_template('form.html')
+    page = "Buat Laporan"
+    return render_template('form.html', page=page)
 
 @app.route('/submit', methods=['POST'])
 def submit():
+    page = "Buat Laporan"
     if request.method == 'POST':
         id_person = generate_random_filename(6)
         nama = request.form['nama']
@@ -61,14 +73,42 @@ def submit():
             'tanggal': tanggal
         }
 
-        db.laporan.insert_one(data)
+        session['kode_akses'] = id_person
 
+        db.laporan.insert_one(data)
+        
+    
+    if 'kode_akses' in session:
         flash('Laporan Anda telah dikirim!', 'success')
-        return redirect(url_for('index'))
+        return render_template('form.html', kode_akses=id_person, page=page)
+    else:
+        return redirect(url_for('form'))
+    
+@app.route('/cari-laporan')
+def c_laporan():
+    page = 'Cari Laporan'
+    return render_template('s-lapor.html', page=page)
+
+@app.route('/cari-laporan/hasil', methods=['POST'])
+def cari():
+    page = 'Cari Laporan'
+    kode_akses = request.form['id_person']
+
+    data = db.laporan.find_one({'id_person': kode_akses})
+
+    session['kode_akses'] = kode_akses
+
+    if data :
+        session['kode_akses'] = True
+        return render_template('s-lapor.html', data=data, id_person=kode_akses, page=page)
+    else:
+        flash('Data laporan tidak ada.', 'error')
+        return redirect(url_for('c_laporan'))
 
 @app.route('/login')
 def login():
-    return render_template('admin/login.html')
+    page = "Sign In"
+    return render_template('admin/login.html', page=page)
 
 @app.route('/login/auth', methods=['GET', 'POST'])
 def login_auth():
@@ -80,10 +120,9 @@ def login_auth():
             flash('Isi kedua kolom username dan password.', 'error')
             return redirect(url_for('login'))
 
-        user = db.admin_user.find_one({'username': username, 'password': password})
+        user = db.admin.find_one({'username': username, 'password': password})
 
         if user:
-            flash('Login berhasil!', 'success')
             session['logged_in'] = True
             key = generate_random_filename(8)
 
@@ -96,29 +135,35 @@ def login_auth():
 
 @app.route('/dashboard/<key>')
 def dashboard(key):
+    page = "Dashboard"
     if not session.get('logged_in'):
         flash('Anda harus login terlebih dahulu!', 'error')
         return redirect(url_for('login'))
     
     data = list(db.laporan.find({}, {'_id': False}))
-    return render_template('admin/dashboard.html', data=data, key=key)
+    return render_template('admin/dashboard.html', data=data, key=key, page=page)
 
-@app.route('/get_data/<key>/<key_person>')
-def get_data(key_person, key):
-    laporan = db.laporan.find_one({"id_person": key_person}, {'_id': False})
+@app.route('/get_data/<key>/<id_person>')
+def get_data(key, id_person):
+    page = "Dashboard"
+
+    laporan = db.laporan.find_one({"id_person": id_person}, {'_id': False})
+
     data = list(db.laporan.find({}, {'_id': False}))
     
-    return render_template('admin/dashboard.html', data=data, selected_laporan=laporan, key=key)
+    return render_template('componens/db/card-laporan.html', data=data, selected_laporan=laporan, key=key, page=page)
     
-@app.route('/update_status/<key_person>', methods=['POST'])
-def update_status(key_person):
+@app.route('/update_status/<id_person>', methods=['POST'])
+def update_status(id_person):
+    page = "Dashboard"
     new_status = 'Sudah'
-    db.laporan.update_one({'id_person': key_person}, {'$set': {'status': new_status}})
-    return redirect(url_for('dashboard', key=key_person))
+    db.laporan.update_one({'id_person': id_person}, {'$set': {'status': new_status}})
+    return redirect(url_for('dashboard', key=id_person, page=page))
 
 
 @app.route('/analisis/<key>')
 def analisis(key):
+    page = "Analisis"
     if not session.get('logged_in'):
         flash('Anda harus login terlebih dahulu!', 'error')
         return redirect(url_for('login'))
@@ -128,7 +173,7 @@ def analisis(key):
     t_sudah = db.laporan.count_documents({"status": "Sudah"})
     
     data = list(db.laporan.find({}, {'_id': False}))
-    return render_template('admin/analisis.html', data=data, key=key,total_d=total_d, t_belum=t_belum, t_sudah=t_sudah)
+    return render_template('admin/analisis.html', data=data, key=key,total_d=total_d, t_belum=t_belum, t_sudah=t_sudah, page=page)
 
 @app.route('/logout')
 def logout():
